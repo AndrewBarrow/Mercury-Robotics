1a43203e923126c35cca5e5facd57fb26f91a3bc
sirdemios <matthewsimon.says@gmail.com>
2016-03-28 21:36:19 +0000

    Added files via upload

---
+++ gamepadstuff/index.js
@@ -0,0 +1,52 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var B=0;
+var L=0;
+var R=0;
+var T=0;
+
+app.get('/', function(req, res){
+   res.sendFile(__dirname + '/gamepadToArduino.html'); //remember eventually this should be the index page again
+});
+app.get('/chat', function(req, res){
+   res.sendFile(__dirname + '/chat.html');
+});
+app.get('/gametest', function(req, res){
+   res.sendFile(__dirname + '/gamepadToArduino.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected to index');
+  socket.on('disconnect', function(){
+    console.log('user left index');
+  });
+});
+  // so apperently there is no rule about reusing the same event, it just adds to the functionallity
+  // so i could combine them but thats super cool and i dont want to forget that
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+     io.emit('chat message', msg);
+	});
+  socket.on('index message', function(msg){
+    console.log('index message: ' + msg);
+	});
+  });
+  function input(label,msg)
+  {
+	  console.log(label+msg);
+  }
+io.on('connection', function(socket){
+		socket.on('message',  function(msg)
+	{
+	  console.log(msg);
+	});
+
+  });
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ gamepadstuff/readme.txt
@@ -0,0 +1,12 @@
+download files and move them to a directory together
+in that directory type
+      npm install
+into the command line
+this will check the package.json object and install the required packages
+then in command line type
+      node chat.js
+which will start the server, connect to the pi via its ip address and port 3000
+ie
+localhost:3000
+or
+192.168.1.xxx:3000
