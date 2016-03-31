393adc316ff35af79cc92b669fc97b32bdeb0482
Blake Barrow <barrow.andrew0@gmail.com>
2016-03-31 15:30:24 -0500

    Added files via upload

---
+++ usb_com_test/usb_com_test.ino
@@ -0,0 +1,61 @@
+#include <led.h>
+#include <motor.h>
+#include <sensor.h>
+
+/*
+  This is a demo for usb communications between Arduino and Raspberry Pi 2.
+*/
+
+//char buffer[8] = { 'a', 'b', 'c', 'd', 'e', 'f', '~', 'þ' };
+char buffer[2] = { };
+int buf;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    /*for (int i = 0; i < 8; i++) {
+      buffer[i] = Serial.read();
+    }*/
+
+      buffer[0] = Serial.read();
+      buffer[1] = Serial.read();
+
+    //String buf(buffer);
+    //Serial.println(buf);
+
+    l.drive(buffer[0], DC::For);
+    r.drive(buffer[1], DC::Rev);
+    
+    //Serial.print(buffer[1]);
+    //Serial.print(buffer[2]);
+    //Serial.print(buffer[3]);
+    //Serial.print(buffer[4]);
+    //Serial.print(buffer[5]);
+    //Serial.print(buffer[6]);
+    //Serial.println(buffer[7]);
+  }
+
+  //buffer_drive();
+
+}
+
+void buffer_drive() {
+  l.drive(buffer[6], DC::Rev);
+  r.drive(buffer[7], DC::For);  
+}
+
+void write_to() {
+  /*for (int i = 0; i < 8; i++) {
+    if (i = 7) {
+      Serial.println(buffer[i]);  
+    } else {
+      Serial.print(buffer[i]);
+    }
+  } */
+  Serial.println("Grapist!");
+  delay(1000);
+}
+  
---
+++ usb_drive_test/usb_drive_test.ino
@@ -0,0 +1,88 @@
+#include <led.h>
+#include <motor.h>
+#include <sensor.h>
+
+/*
+  This is a demo for usb communications between Arduino and Raspberry Pi 2.
+*/
+
+char buffer[8] = { };
+
+struct usb_input {
+  unsigned int digital;
+  unsigned int ls;
+  unsigned int rs;
+  unsigned char lt;
+  unsigned char rt;  
+};
+
+bool motor_zero = false;
+
+usb_input ui;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    for (int i = 0; i < 8; i++) {
+      buffer[i] = Serial.read();
+    }
+
+  ui.digital = ((buffer[0] << 8) + buffer[1]);
+  ui.ls = ((buffer[2] << 8) + buffer[3]);
+  ui.rs = ((buffer[4] << 8) + buffer[5]);
+  ui.lt = buffer[6];
+  ui.rt = buffer[7];
+
+  if ((ui.lt) || (ui.rt)) {
+    if ((ui.rt > 0) && !(ui.lt > 0)) {
+      l.drive(ui.rt, DC::For);
+      r.drive(ui.rt, DC::For);
+      motor_zero = true;  
+    }
+  } else if (motor_zero == true){
+    l.drive(0, l.prev_dir());
+    r.drive(0, l.prev_dir());
+    motor_zero = false;  
+  }
+    
+
+   //for (int i = 0; i < 8; i++) {
+   //   Serial.print(buffer[i]);
+   // }
+
+   Serial.print(ui.rt);
+
+/*
+Serial.print(buffer[1], HEX);
+Serial.print(" ");
+Serial.print(buffer[2], HEX);
+Serial.print(" ");
+Serial.print(buffer[3], HEX);
+Serial.print(" ");
+Serial.print(buffer[4], HEX);
+Serial.print(" ");
+Serial.print(buffer[5], HEX);
+Serial.print(" ");
+Serial.print(buffer[6], HEX);
+Serial.print(" ");
+Serial.print(buffer[7], HEX);
+Serial.print(" ");
+Serial.println(buffer[8], HEX);
+*/
+/*
+Serial.print(ui.digital, HEX);
+Serial.print(" ");
+Serial.print(ui.ls, HEX);
+Serial.print(" ");
+Serial.print(ui.rs, HEX);
+Serial.print(" ");
+Serial.print(ui.lt, HEX);
+Serial.print(" ");
+Serial.println(ui.rt, HEX);
+*/
+  }
+}
+  
