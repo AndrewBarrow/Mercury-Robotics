f47cbe0ffa8cd1d0881f4287a7505808cbdae388
sirdemios <matthewsimon.says@gmail.com>
2016-04-04 13:58:56 -0500

    Update second.sh

--- setupscripts/second.sh
+++ setupscripts/second.sh
@@ -1,9 +1,3 @@
-wget http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u3_armhf.deb
- wget http://launchpadlibrarian.net/218525709/chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
- wget http://launchpadlibrarian.net/218525711/chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
- sudo dpkg -i libgcrypt11_1.5.0-5+deb7u3_armhf.deb
- sudo dpkg -i chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
- sudo dpkg -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
 sudo apt-get install geany -y
 
 sudo apt-get update
@@ -12,3 +6,10 @@ sudo apt-get install uv4l uv4l-raspicam -y
 sudo apt-get install uv4l-raspicam-extras -y
 
 sudo rpi-update
+
+wget http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u3_armhf.deb
+ wget http://launchpadlibrarian.net/218525709/chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+ wget http://launchpadlibrarian.net/218525711/chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+ sudo dpkg -i libgcrypt11_1.5.0-5+deb7u3_armhf.deb
+ sudo dpkg -i chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
+ sudo dpkg -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
