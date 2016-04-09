b5913e41c477c8ba82fd1e0f89522e739c7b8dd9
sirdemios <matthewsimon.says@gmail.com>
2016-04-09 13:56:59 -0500

    Update second.sh

--- setupscripts/second.sh
+++ setupscripts/second.sh
@@ -7,9 +7,9 @@ sudo apt-get install uv4l-raspicam-extras -y
 
 sudo rpi-update
 
-wget http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u3_armhf.deb
+wget http://ftp.us.debian.org/debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.0-5+deb7u4_armhf.deb
  wget http://launchpadlibrarian.net/218525709/chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
  wget http://launchpadlibrarian.net/218525711/chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
- sudo dpkg -i libgcrypt11_1.5.0-5+deb7u3_armhf.deb
+ sudo dpkg -i libgcrypt11_1.5.0-5+deb7u4_armhf.deb
  sudo dpkg -i chromium-codecs-ffmpeg-extra_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
  sudo dpkg -i chromium-browser_45.0.2454.85-0ubuntu0.14.04.1.1097_armhf.deb
